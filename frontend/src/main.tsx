5352418b3fd6e32554197560114b44335149dc1c
joselyne.nieves <joselyne.nievesr@gmail.com>
2025-03-18 21:44:54 -0300

    feat(main): agregar configuración de los providers, rutas y manejo de estados
    - Agregar Proveedor de React Query
    - Agregar proveedor del tema
    - Agregar proveedor de la notificación
    - Agregar React router

---
+++ frontend/src/main.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './theme/ThemeContext';
+import { NotificationProvider } from './contexts/NotificationContext';
+import App from './App';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <NotificationProvider>
+          <BrowserRouter
+            future={{
+              v7_startTransition: true,
+              v7_relativeSplatPath: true,
+            }}
+          >
+            <App />
+          </BrowserRouter>
+        </NotificationProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
