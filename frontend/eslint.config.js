cf438f4c66fbc487ff00d0ddc2d4ec6190f470ce
joselyne.nieves <joselyne.nievesr@gmail.com>
2025-03-19 12:49:39 -0300

    chore(eslint): actualización de paquetes y dependencias de eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,35 +3,34 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tsESLintPlugin from '@typescript-eslint/eslint-plugin';
-const { configs } = tsESLintPlugin;
+import tsESLintParser from '@typescript-eslint/parser';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  { ignores: ['dist'] },  // Ignorar la carpeta dist
+  { ignores: ['dist'] },
   js.configs.recommended,
-  configs.strictTypeChecked,
-  configs.stylisticTypeChecked,
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx}'], // Aplicar estas reglas solo a archivos TypeScript
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser, // Variables globales del navegador
+      parser: tsESLintParser, // Usar el parser de TypeScript
       parserOptions: {
-        project: './tsconfig.json'
-      }
+        project: './tsconfig.json', // Usar el archivo tsconfig.json
+      },
     },
     plugins: {
-      '@typescript-eslint': tsESLintPlugin,
-      reactHooks,
-      reactRefresh,
-      prettier: prettierPlugin
+      '@typescript-eslint': tsESLintPlugin, // Plugin de TypeScript
+      'react-hooks': reactHooks, // Plugin de React Hooks
+      'react-refresh': reactRefresh, // Plugin de React Refresh
+      prettier: prettierPlugin, // Plugin de Prettier
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': ['error', { endOfLine: 'auto' }]
+      '@typescript-eslint/no-unused-vars': 'warn', // Advertir sobre variables no usadas
+      'react-hooks/rules-of-hooks': 'error', // Error en reglas de Hooks
+      'react-hooks/exhaustive-deps': 'warn', // Advertir sobre dependencias de Hooks
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }], // Advertir sobre exportaciones no válidas
+      'prettier/prettier': ['error', { endOfLine: 'auto' }], // Integración con Prettier
     },
   },
 ];
